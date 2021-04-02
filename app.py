2cbedc32056635fc6e73af086630c5c3d3b18e78
Ian Arch <ian.arch01@gmail.com>
2021-04-01 19:47:45 -0600

    final app.py changes

--- app.py
+++ app.py
@@ -33,15 +33,15 @@ def Home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-    results = session.query(measure.date, measure.prcp).all()
+    precip_query = session.query(measure.date, measure.prcp).all()
     session.close()
-    dateprecip_list = []
-    for date, prcp in results:
+    precip_list = []
+    for date, prcp in precip_query:
         dpdict = {}
         dpdict["date"] = date
         dpdict["prcp"] = prcp
-        dateprecip_list.append(dpdict)
-    return jsonify(dateprecip_list)
+        precip_list.append(dpdict)
+    return jsonify(precip_list)
 
 #Stations - JSON list of stations
 @app.route("/api/v1.0/stations")
@@ -53,10 +53,52 @@ def stations():
     return jsonify(stations_list)
 
 #Tobs - query results & JSON list
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    tobs_query = session.query(measure.date, measure.tobs).filter(measure.date >= '2016-08-23').filter(measure.station=='USC00519281').all()
+    session.close()
+    tobs_list = []
+    for date, tobs in tobs_query:
+        dtdict = {}
+        dtdict["date"] = date
+        dtdict["tobs"] = tobs
+        tobs_list.append(dtdict)
+    return jsonify(tobs_list)
 
+#Start - JSON list, TMIN/TAVG/TMAX for both
+@app.route("/api/v1.0/start")
+def date_start(start):
+    session = Session(engine)
+    start_query = session.query(func.min(measure.tobs),func.avg(measure.tobs),func.max(measure.tobs)).filter(measure.date >= start).all()
+    session.close
+    start_list = []
+    for min,avg,max in start_query:
+        sdict = {}
+        sdict["Min"] = min
+        sdict["Average"] = avg
+        sdict["Max"] = max
+        start_list.append(sdict)
+    return jsonify(start_list)
 
-#Start & Start/End - JSON list, TMIN/TAVG/TMAX for both
+#could not get to work show... want to come back and review, I believe code is correct...
 
+#Start/End - JSON list, TMIN/TAVG/TMAX for both
+@app.route("/api/v1.0/start/end")
+def date_startend(start,end):
+    session = Session(engine)
+    startend_query = session.query(func.min(measure.tobs),func.avg(measure.tobs),func.max(measure.tobs)).filter(measure.date >= start, measure.date <= end).all()
+    session.close
+    startend_list = []
+    for min,avg,max in start_query:
+        sedict = {}
+        sedict["Min"] = min
+        sedict["Average"] = avg
+        sedict["Max"] = max
+        startend_list.append(sedict)
+    return jsonify(startend_list)
+
+#could not get to work show... want to come back and review, I believe code is correct...
 
 if __name__ == "__main__":
     app.run(debug=True)
